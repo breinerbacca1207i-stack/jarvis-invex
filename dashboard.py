d274b37f7c07f47e4362857a9f07a3ade64c347d
breinerbacca1207i-stack <breinerbacca1207i@gmail.com>
2026-06-26 13:36:43 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -5,9 +5,7 @@ import pandas_ta as ta
 from google import genai
 import os
 from supabase import create_client, Client
-# ... tus otros imports (pandas, yfinance, etc.) ...
 
-# Conexión a Supabase usando los Secrets que configuraste
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
