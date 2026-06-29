1fb1a62edf25dd3ccc19f7f3f4664bd19e51a61d
breinerbacca1207i-stack <breinerbacca1207i@gmail.com>
2026-06-28 21:40:38 -0500

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -16,7 +16,7 @@ st.title("Invex: Asistente Cuantitativo JARVIS")
 st.markdown("---")
 
 # Conexión segura a Gemini
-API_KEY_GEMINI = os.getenv("API_KEY_GEMINI", st.secrets.get("API_KEY_GEMINI", "AQ.Ab8RN6KOir4bSzsF42EPYHv8fsv4Yi1uwX4T25TX-awSFXiwug"))
+API_KEY_GEMINI = os.getenv("API_KEY_GEMINI", st.secrets.get("API_KEY_GEMINI", "TU_API_KEY_GEMINI"))
 
 # --- 2. MOTOR DE ANÁLISIS TÉCNICO (Oculto al usuario) ---
 def obtener_radiografia_tecnica(ticker):
